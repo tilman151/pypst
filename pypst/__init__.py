6c33e6df9aa93f18e959d665da7d4a2631291e61
Tilman Krokotsch <t.krokotsch@gmail.com>
2024-08-15 10:14:52 +0200

    refactor: add element imports in root init file (#6)

--- pypst/__init__.py
+++ pypst/__init__.py
@@ -2,6 +2,18 @@ from pypst.table import Table
 from pypst.cell import Cell
 from pypst.figure import Figure
 from pypst.document import Document
+from pypst.heading import Heading
+from pypst.itemize import Itemize, Enumerate
+from pypst.image import Image
 
-__all__ = ["Table", "Cell", "Figure", "Document"]
+__all__ = [
+    "Table",
+    "Cell",
+    "Figure",
+    "Document",
+    "Heading",
+    "Itemize",
+    "Enumerate",
+    "Image",
+]
 __version__ = "0.1.0"
