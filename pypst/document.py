a9679a6b711d15b42f601225422d4d3894f5e514
Tilman Krokotsch <t.krokotsch@gmail.com>
2024-08-15 10:35:29 +0200

    docs: add document docstrings

--- pypst/document.py
+++ pypst/document.py
@@ -6,12 +6,50 @@ from pypst.renderable import Renderable
 
 
 class Document:
+    """
+    Represents a Typst document serving as the top-level container.
+
+    A document contains other renderable objects and necessary imports.
+    It is only necessary if multiple renderable objects need to be combined
+    or third-party files need to be imported.
+
+    Examples:
+            >>> doc = Document("Hello, world!")
+            >>> doc.render()
+            "Hello, world!"
+
+            >>> doc = Document(["Hello,", "world!"])
+            >>> doc.render()
+            "Hello,\\n\\nworld!"
+
+            >>> doc = Document()
+            >>> doc.add("Hello,")
+            >>> doc.add("world!")
+            >>> doc.render()
+            "Hello,\\n\\nworld!"
+
+            >>> doc = Document()
+            >>> doc.add_import("module")
+            >>> doc.add("Hello, world!")
+            >>> doc.render()
+            "#import \"module\"\\n\\nHello, world!"
+    """
+
     _body: list[Renderable | str]
     imports: list["Import"]
 
     def __init__(
         self, body: Optional[Renderable | str | list[Renderable | str]] = None
     ) -> None:
+        """
+        Create a new document with an optional body.
+
+        The body can be a single renderable object, a string,
+        or a list of renderable objects and strings.
+
+        Args:
+            body: The body of the document.
+        """
         self._body = []
         self.imports = []
 
@@ -24,22 +62,57 @@ class Document:
 
     @property
     def body(self) -> list[Renderable | str]:
+        """The body of the document."""
         return self._body
 
-    def add(self, body: Renderable | str) -> None:
-        if not isinstance(body, (Renderable, str)):
-            raise ValueError(f"Invalid body type: {type(body)}")
-        elif isinstance(body, Document):
+    def add(self, element: Renderable | str) -> None:
+        """
+        Add a body element to the document.
+
+        Any object of the `pypst` library with a `render` method can be added.
+        Simple text paragraphs or unsupported Typst elements can be added as
+        strings.
+        Adding an `Import` object is equivalent to calling `add_import`.
+
+        Args:
+            element: The new body element.
+        """
+        if not isinstance(element, (Renderable, str)):
+            raise ValueError(f"Invalid body type: {type(element)}")
+        elif isinstance(element, Document):
             raise ValueError("Document cannot be set as value of another document")
-        elif isinstance(body, Import):
-            self.imports.append(body)
+        elif isinstance(element, Import):
+            self.imports.append(element)
         else:
-            self._body.append(body)
+            self._body.append(element)
 
     def add_import(self, module: str, members: Optional[list[str]] = None) -> None:
+        """
+        Add an import to the document.
+
+        If members are provided,
+        only those members will be imported as `#import <module>: <list of members>`.
+        If no members are provided, the entire module will be imported as
+        `#import <module>`.
+        Imports are rendered at the top of the document.
+
+        Args:
+            module: The module to import.
+            members: The optional list of members to import.
+        """
         self.imports.append(Import(module, members or []))
 
     def render(self) -> str:
+        """
+        Render the document.
+
+        The rendered document has all imports at the top followed by the body.
+        Each element in the body is rendered in the order it was added.
+        A blank line separates each body element.
+
+        Returns:
+            The rendered document string.
+        """
         imports = "\n".join([i.render() for i in self.imports])
         body = "\n\n".join(utils.render(b) for b in self.body)
 
