2b1a229f4f94e7d69a138bd8e8316241d4640d6b
Tiemen Schuijbroek <t.j.l.schuijbroek@ratio-case.nl>
2025-01-08 15:13:30 +0100

    Rename test_set to test_set_rule tests

--- tests/test_set_rule.py
+++ tests/test_set_rule.py
@@ -3,13 +3,13 @@ import pytest
 from pypst import Content, Document, SetRule
 
 
-def test_set():
+def test_set_rule():
     obj = SetRule(selector="text", arguments={"fill": "red", "size": "18pt"})
     assert obj.render() == "#set text(fill: red, size: 18pt)"
 
 
 @pytest.mark.integration
-def test_set_compile(test_compile):
+def test_set_rule_compile(test_compile):
     obj = SetRule(selector="text", arguments={"fill": "red", "size": "18pt"})
     doc = Document([obj, Content("Large and red text.")])
     test_compile(doc)
