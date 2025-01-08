2b73530456fb2d2d55d20c7d7b339db1922c1caf
Tilman Krokotsch <t.krokotsch@gmail.com>
2025-01-08 09:38:32 +0000

    refactor: mypy issues

--- pypst/renderable.py
+++ pypst/renderable.py
@@ -20,7 +20,7 @@ class Renderable(ABC):
             if any("render" in B.__dict__ for B in subclass.__mro__):
                 return True
 
-        return NotImplemented
+        return NotImplemented # type: ignore[no-any-return]
 
 
 @dataclass
